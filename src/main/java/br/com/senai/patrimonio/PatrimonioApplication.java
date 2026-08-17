083aa98511af6bd8e78545f6cd98711c78d92982
simon_clemenceau <simon_clemenceau@estudante.sesisenai.org.br>
2026-08-17 19:05:34 -0300

    Criado Endereco

--- src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
+++ src/main/java/br/com/senai/patrimonio/PatrimonioApplication.java
@@ -1,5 +1,6 @@
 package br.com.senai.patrimonio;
 
+import br.com.senai.patrimonio.model.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,5 @@ public class PatrimonioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatrimonioApplication.class, args);
 	}
-
 }
+
--- src/main/java/br/com/senai/patrimonio/model/Endereco.java
+++ src/main/java/br/com/senai/patrimonio/model/Endereco.java
@@ -7,7 +7,14 @@ public class Endereco {
     private String bairro;
     private String estado;
 
-    public Endereco(){
+    public Endereco(){}
+
+    public Endereco(String rua, String numero, String complemento, String bairro, String estado) {
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.estado = estado;
     }
 
     public String getRua() {
@@ -16,5 +23,37 @@ public class Endereco {
 
     public void setRua(String rua) {
         this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
